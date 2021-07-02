a0efa0a982f88f25b51002675ebad9aa409af92f
Tobias Andersson <siesta2007@hotmail.com>
2021-07-02 15:14:25 +0900

    Frontend for TicTacToe (Not connected to backend yet)

---
+++ frontend/src/App.js
@@ -0,0 +1,107 @@
+import './App.css';
+import React from "react";
+
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}
+      style={{
+        borderWidth:1,
+        borderColor:'rgba(0,0,0,0.2)',
+        alignItems:'center',
+        justifyContent:'center',
+        width:100,
+        height:100,
+        backgroundColor:'#EEE',
+        flex: 1,
+        color: props.value === "." ? '#EEE' : '#333',
+        fontSize: 70,
+      }}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill("."), // TODO: This will be fetch from api.
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return <Square 
+      value={this.state.squares[i]} 
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+  render() {
+    
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+      
+    <div className="App">
+      <header className="App-header">
+        <Game />
+      </header>
+    </div>
+  );
+}
+
+export default App;
